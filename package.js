81eb6314bb42c8b3370aa42abfa4679e07610e54
Jérémy "Vrakfall" Lecocq <jeremy@artphotolaurent.be>
2017-01-18 01:30:32 +0100

    Removed unnecessary api.mainModule() call

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe( {
-  name: 'phaser',
+  name: 'vrakfall:phaser',
   version: '2.6.2-0',
   summary: 'Wrapper around Phaser to make it usable in Meteor.',
   git: 'https://github.com/Vrakfall/meteor-phaser',
@@ -8,7 +8,6 @@ Package.describe( {
 
 Package.onUse( function ( api ) {
   api.versionsFrom( '1.0' );
-  api.mainModule( 'phaser.js' );
   api.addFiles( 'node_modules/phaser/build/phaser.min.js', 'client', {
     bare: true
   } );
